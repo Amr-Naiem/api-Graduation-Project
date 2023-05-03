f5d30758ee9c14c38158520494309c1032926ea4
Amr-Naiem <amrnaeim512@gmail.com>
2023-05-03 23:56:42 +0300

    added jwt to authProvider

--- routes/authProvider.js
+++ routes/authProvider.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const Provider = require("../models/Provider");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// a function to generate a JWT token when a user logs in successfully:
+function generateToken(provider) {
+  const expiresIn = '365d'; // set the expiration time to 1 year
+  return jwt.sign({ _id: provider._id }, process.env.JWT_SECRET);
+}
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -18,7 +25,8 @@ router.post("/register", async (req, res) => {
     });
 
     const provider = await newProvider.save();
-    res.status(200).json(provider);
+    const token = generateToken(provider);
+    res.status(200).json({ provider, token });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -27,19 +35,19 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-
     const provider = await Provider.findOne({ username: req.body.username });
-    !provider && res.status(400).json("Wrong credentials!");
-    if (provider) {
-      const validated = await bcrypt.compare(req.body.password, provider.password);
-      !validated && res.status(400).json("Wrong credentials!")
-      
-      const { password, ...others } = provider._doc;
-      (Provider && validated) && res.status(200).json(others);
-    } 
-    
-    //!validated && res.status(400).json("Wrong credentials!");
+    if (!provider) {
+      return res.status(400).json("Wrong credentials!");
+    }
 
+    const validated = await bcrypt.compare(req.body.password, provider.password);
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const token = generateToken(provider);
+    const { password, ...others } = provider._doc;
+    res.status(200).json({ provider: others, token });
   } catch (err) {
     res.status(500).json(err);
   }
