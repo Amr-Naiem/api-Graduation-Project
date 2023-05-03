ddc8f53ba6834914183a82e84d64aec4e87703f3
Amr-Naiem <amrnaeim512@gmail.com>
2023-05-04 01:13:31 +0300

    added jwt to services

--- routes/services.js
+++ routes/services.js
@@ -2,14 +2,32 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Service = require("../models/Service");
 const Provider = require("../models/Provider");
+const jwt = require("jsonwebtoken");
+
+// a middleware function to verify JWT tokens:
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json("Access denied!");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.providerId = decoded._id;
+    next();
+  } catch (err) {
+    res.status(400).json("Invalid token!");
+  }
+}
 
 //CREATE SERVICE
-router.post("/create", async (req, res) => {
-  const newService = new Service(req.body);
-  const provider = await Provider.findOne({username: req.body.provider});
+router.post("/create", verifyToken, async (req, res) => {
   try {
+    const provider = await Provider.findOne({username: req.body.provider});
     if(provider && (provider.username === req.body.provider))
     {
+      const newService = new Service(req.body);
       const savedService = await newService.save();
       res.status(200).json(savedService);
     } else {
@@ -21,8 +39,7 @@ router.post("/create", async (req, res) => {
 });
 
 //UPDATE SERVICE
-router.put("/:id", async (req, res) => {
-  //const provider = await Provider.findOne({username: req.body.provider});
+router.put("/:id", verifyToken, async (req, res) => {
   try {
     const service = await Service.findById(req.params.id);
     if (service.provider === req.body.provider) {
@@ -47,7 +64,7 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE SERVICE
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", verifyToken, async (req, res) => {
   try {
     const service = await Service.findById(req.params.id);
     if (service.provider === req.body.provider) {
