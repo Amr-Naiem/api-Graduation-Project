f6ce47fa7cb363c06cb8513dfa9dc9b4c9bf3fd4
Amr-Naiem <amrnaeim512@gmail.com>
2023-05-08 11:39:21 +0300

    get new providers and add accept or reject them

--- routes/providers.js
+++ routes/providers.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Provider = require("../models/Provider");
+const NewProviders = require("../models/newProviders");
 const Service = require("../models/Service");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -70,6 +71,46 @@ router.get("/:id", verifyToken, async (req, res) => {
     const provider = await Provider.findById(req.params.id);
     const { password, ...others } = provider._doc;
     res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL NEW PROVIDERS
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const newproviders = await NewProviders.find();
+    res.status(200).json(newproviders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ACCEPT NEW PROVIDER
+router.put("/accept/:id", verifyToken, async (req, res) => {
+  try {
+    const newProvider = await NewProviders.findOne({ _id: req.params.id });
+    if (!newProvider) {
+      return res.status(404).json("New provider not found!");
+    }
+    newProvider.status = "accepted";
+    await newProvider.save();
+    res.status(200).json(newProvider);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// REJECT NEW PROVIDER
+router.put("/reject/:id", verifyToken, async (req, res) => {
+  try {
+    const newProvider = await NewProviders.findOne({ _id: req.params.id });
+    if (!newProvider) {
+      return res.status(404).json("New provider not found!");
+    }
+    newProvider.status = "rejected";
+    await newProvider.save();
+    res.status(200).json("New provider has been rejected...");
   } catch (err) {
     res.status(500).json(err);
   }
